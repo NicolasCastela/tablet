4b1e7233f91b426a2f0fc952beeb831fc0667b3f
nickcastela2022@hotmail.com <nickcastela2022@hotmail.com>
2023-10-27 16:05:36 -0400

    allan salva vidas

--- src/views/Pessoas.vue
+++ src/views/Pessoas.vue
@@ -7,9 +7,9 @@
       hide-details></v-text-field>
     <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
 
-    <div v-for="(person, index) in pessoas.data.personInfos" :key="index">
+    <div v-for="(person, index) in personList" :key="index">
       <v-card class="mx-auto mt-5" max-width="344">
-        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img> 
+        <v-img :src="selectImageForPerson(person.id)" height="200px" cover></v-img>
         <v-card-title>
           Nome: {{ person.name }}
         </v-card-title>
@@ -27,50 +27,65 @@ export default {
   data() {
     return {
       personImgList: [],
-      pessoas: {}
+      personList:[]
 
     }
   },
-  // methods: {
-  //   mostrarFaces() {
-  //     const pass = '12345678';
-  //     const personId = '1';
-  //     fetch(`http://192.168.15.34:8090/face/find?pass=${pass}&personId=${personId}`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/x-www-form-urlencoded'
-  //       }
-  //     })
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         // const imageList = data.map((item) => item.path)
-  //         // const imageList = data
-  //         this.personImgList = data
-  //         // console.log(imageList)
+  methods: {
+    findProfilePicture(personId) {
+      console.log(personId)
+      const pass = '12345678';
+      fetch(`http://192.168.15.34:8090/face/find?pass=${pass}&personId=${personId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((respostaJson) => {
+          
+          console.log(respostaJson.data[0]);
 
+          const firtPictureForThisPerson = respostaJson.data[0];
 
-  //         // if (pessoa) {
-  //         //   pessoa.faces = data.data;
-  //         //   console.log(pessoa.faces);
-  //         // }
-  //       })
-  //       .catch(error => {
-  //         console.error('Erro ao buscar faces:', error);
-  //       });
-  //   }
-  // },
+          const personPicture = {
+            id: firtPictureForThisPerson.personId,
+            // imageUrl: firtPictureForThisPerson.path,
+            imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          };
+
+          this.personImgList.push(personPicture);
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar faces:", error);
+        });
+    },
+    selectImageForPerson(personId) {
+      console.log(personId);
+      // Pegar imagem correspondente do This.personImgList
+    },
+  },
 
   mounted() {
-    
+
     fetch('http://192.168.15.34:8090/person/findByPage?personId=-1&length=-1&pass=12345678&index=0  ', {
       method: 'GET'
     })
       .then((resposta) => resposta.json())
-      .then((data) => {
-        this.pessoas = data
-                console.log('console pessoas', data)
+      .then((respostaJson) => {
+        const findedPersons = respostaJson.data.personInfos;
+
+        this.personList = findedPersons;
+
+        for (const person of findedPersons) {
+          const personId = person.id;
+          this.findProfilePicture(personId);
+        }
       })
-  },
+      
+  }
 
 };
 </script>
